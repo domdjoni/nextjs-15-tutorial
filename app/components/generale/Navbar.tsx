c018fe38bb0e65157eacf38f8337ce75a8145905
Kristijan Domdjoni <kristijandomdjoni@gmail.com>
2025-04-10 18:16:13 +0200

    added post install

--- app/components/generale/Navbar.tsx
+++ app/components/generale/Navbar.tsx
@@ -1,6 +1,6 @@
 
 
-import {  buttonVariants } from '@/components/ui/button'
+
 import { LoginLink, LogoutLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/components'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import Link from 'next/link'
--- app/components/generale/Submitbutton.tsx
+++ app/components/generale/Submitbutton.tsx
@@ -1,5 +1,6 @@
 
 
+
 import { Button } from '@/components/ui/button'
 import React from 'react'
 import { useFormStatus } from 'react-dom'
