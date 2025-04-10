8d6952adab1172b925c15b4b30144705d2a19224
Kristijan Domdjoni <kristijandomdjoni@gmail.com>
2025-04-10 18:30:57 +0200

    added post install

--- app/components/generale/Navbar.tsx
+++ app/components/generale/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = async () => {
           ) :(
                <div className='flex items-center gap-4'>
                     <LoginLink>Login</LoginLink>
-                    <RegisterLink >Sign Up</RegisterLink>
+                    <RegisterLink>Sign Up</RegisterLink>
 
                </div>
           )
