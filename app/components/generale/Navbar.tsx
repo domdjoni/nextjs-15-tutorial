897376f9f3aeccff69c148854b531b2374776b43
Kristijan Domdjoni <kristijandomdjoni@gmail.com>
2025-04-10 18:36:56 +0200

    added post install

--- app/components/generale/Navbar.tsx
+++ app/components/generale/Navbar.tsx
@@ -2,7 +2,7 @@
 
 
 
-import { buttonVariants } from '@/components/ui/button'
+
 import { LoginLink, LogoutLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/components'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import Link from 'next/link'
@@ -39,7 +39,7 @@ const Navbar = async () => {
           user ? (
                <div className='flex items-center gap-4'>
                     {user.given_name}
-                    <LogoutLink className={buttonVariants({variant:"secondary"})}>Logout</LogoutLink>
+                    <LogoutLink >Logout</LogoutLink>
                </div>
           ) :(
                <div className='flex items-center gap-4'>
